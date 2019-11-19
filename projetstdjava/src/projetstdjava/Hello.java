b9f49ec2b31f49b6e3ac3433647343ec0013d019
germain72 <a-vos-clics@wanadoo.fr>
2019-11-19 16:16:05 +0100

    V01

---
+++ projetstdjava/src/projetstdjava/Hello.java
@@ -0,0 +1,14 @@
+package projetstdjava;
+/**
+ * 
+ * @author Administrateur
+ *
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Projet Maven Vide !");
+	}
+
+}
---
+++ projetstdjava/src/projetstdjava/Test01.java
@@ -0,0 +1,14 @@
+package projetstdjava;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test01 {
+
+	@Test
+	public void testMain() {
+		//fail("Not yet implemented");
+	}
+
+}
